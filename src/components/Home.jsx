464ca1e12ba767aa37f09ed4f13b9305a9df29de
firegloves <lucorset@gmail.com>
2024-02-23 19:30:25 +0100

    github link component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Terminal from "./terminal/Terminal.jsx";
 import {TerminalProvider} from "./terminal/TerminalContext.jsx";
 import Loader from "./Loader.jsx";
 import Photo from "./Photo.jsx";
+import GitHubLink from "./GitHubLink.jsx";
 
 const Home = () => {
 
@@ -24,29 +25,32 @@ const Home = () => {
         <Loader imageLoaded={imageLoaded}></Loader>
 
         {imageLoaded && (
-            <div
-                className="main absolute w-full flex flex-col items-center h-screen pt-[150px] bg-[url('/bg-home.jpg')] bg-center bg-cover fadeIn"
-                style={{'--fade-delay': '0.2s'}}>
-
-              <Photo cssClasses={'fadeIn z-[1] top-2'}></Photo>
-
+            <div>
               <div
-                  className="bg-green-600 text-white font-bold py-2 px-4 rounded-full justify-center items-center border-2 border-white fadeIn"
-                  style={{'--fade-delay': '0.6s'}}>
-                Luca Corsetti - Software Engineer
+                  className="main absolute w-full flex flex-col items-center h-screen pt-[150px] bg-[url('/bg-home.jpg')] bg-center bg-cover fadeIn"
+                  style={{'--fade-delay': '0.2s'}}>
+
+                <Photo cssClasses={'fadeIn z-[1] top-2'}></Photo>
+
+                <div
+                    className="bg-green-600 text-white font-bold py-2 px-4 rounded-full justify-center items-center border-2 border-white fadeIn"
+                    style={{'--fade-delay': '0.6s'}}>
+                  Luca Corsetti - Software Engineer
+                </div>
+
+                <button
+                    className="bg-green-600 text-white font-bold text-4xl px-10 mt-[50px] rounded-sm border-2 border-white animate-pulse"
+                    // style={{ opacity: isButtonVisible ? '1' : '0' }}
+                    onClick={() => setIsTerminalOpen(true)}>
+                  T
+                </button>
+
+                {isTerminalOpen &&
+                    <TerminalProvider>
+                      <Terminal closeModal={() => setIsTerminalOpen(false)}/>
+                    </TerminalProvider>}
               </div>
-
-              <button
-                  className="bg-green-600 text-white font-bold text-4xl px-10 mt-[50px] rounded-sm border-2 border-white animate-pulse"
-                  // style={{ opacity: isButtonVisible ? '1' : '0' }}
-                  onClick={() => setIsTerminalOpen(true)}>
-                T
-              </button>
-
-              {isTerminalOpen &&
-                  <TerminalProvider>
-                    <Terminal closeModal={() => setIsTerminalOpen(false)}/>
-                  </TerminalProvider>}
+              <GitHubLink></GitHubLink>
             </div>
         )}
       </div>
